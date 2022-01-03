0a4f04eddcf892eaf7e4123f27d27796fb6d86e9
Mashniskuy Evhene <mashniskuy@gmail.com>
2022-01-03 17:09:51 +0200

    databinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root',//could be [app-root],'.app-root'
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-app';
+  name = 'Eugen';
 }
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'initial name';
+  constructor() {
+    setTimeout(() =>{
+      this.allowNewServer=true;
+    },2000)
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus = 'Server was created!' + this.serverName;
+  }
+
+  onUpdateServerName(event : any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  ngOnInit(): void {
+  }
+
+}
